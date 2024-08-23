60ca7ffcc305249b400f905bba24d78986d929f0
User <User@ARVHINTH-ARCHANAA>
2024-08-23 19:40:17 +0530

    commit 1

--- C43_Archanaa/src/com/tnsif_day1/MyfirstProgram/MyfirstProgram.java
+++ C43_Archanaa/src/com/tnsif_day1/MyfirstProgram/MyfirstProgram.java
@@ -3,8 +3,8 @@ package com.tnsif_day1.MyfirstProgram;
 public class MyfirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("ammu");
+		// TODO Auto-generated method stub
+
 	}
 
 }
