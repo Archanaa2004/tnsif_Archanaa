041ab0764f3a2d0d81f934fe20a670db221d1e19
User <User@ARVHINTH-ARCHANAA>
2024-08-23 20:29:40 +0530

    commit 1

--- C43_Archanaa/src/com/tnsif/day5/ConstructorScanner/ScannerClass.java
+++ C43_Archanaa/src/com/tnsif/day5/ConstructorScanner/ScannerClass.java
@@ -3,7 +3,7 @@ package com.tnsif.day5.ConstructorScanner;
 import java.util.Scanner;
 
 public class ScannerClass {
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 			
 			Scanner sc = new Scanner(System.in); // Syntax for the scanner class
 			System.out.println("Enter your name:");
